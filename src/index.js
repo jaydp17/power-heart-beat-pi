8ee887ac35f29ac6dfcefececb12b1017077cd0d
Jaydeep Solanki <jaydp17@gmail.com>
2017-08-19 20:28:38 +0530

    separate out prefix for storing dev & prod data

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@
 require('dotenv').config();
 
 const moment = require('moment');
+const os = require('os');
 
 const firebase = require('./firebase');
 const auth = require('./auth');
 const utils = require('./utils');
+
+const isProd = process.env.NODE_ENV === 'production' || os.hostname() === 'raspberrypi';
+const dbPrefix = isProd ? 'powerData' : 'dev-powerData';
 
 main();
 
@@ -23,9 +27,9 @@ async function sendHeartBeat() {
   await auth.ensureAuthenticated();
 
   // make a nested structure of date/time
-  const key = moment().format('YYYY/MM/DD/[time]/HH/mm');
+  const key = moment().format('YYYY/MM/DD/Z/HH/mm');
 
   console.log(`sending to ${key}`);
-  await firebase.database().ref(`powerData/${key}`).set(1);
+  await firebase.database().ref(`${dbPrefix}/${key}`).set(1);
   console.log('sent');
 }
